ce8ec0eaee103aad36300940ca7406c4d94f36c8
Fikri <adhitamafikri.developer@gmail.com>
2026-03-02 20:50:45 +0700

    initial commit

---
+++ src/views/MouseMovement.vue
@@ -0,0 +1,11 @@
+<template>
+  <section>
+    <h1>Mouse Movement View</h1>
+    <p>Mouse position: X: {{ x }}, Y: {{ y }}</p>
+  </section>
+</template>
+
+<script setup lang="ts">
+import { useMouse } from "@vueuse/core";
+const { x, y } = useMouse();
+</script>
